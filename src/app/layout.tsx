c68a2bda86479fd48e099a992a1f6e8124ea7c5e
Inkerrr <artemlysyansky@gmail.com>
2026-02-25 23:35:33 +0200

    apollo test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 import { DM_Sans, Geist_Mono } from "next/font/google";
 import { FacebookSDK } from "@/components/FacebookSDK";
 import { PostHogProvider } from "@/components/PostHogProvider";
@@ -30,6 +31,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script
+            id="apollo-tracker"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,o.onload=function(){window.trackingFunctions.onLoad({appId:"699f55ac819bb7001d8326ce"})},document.head.appendChild(o)}initApollo();`,
+            }}
+          />
+        </head>
         <body
           className={`${dmSans.className} ${geistMono.variable} antialiased`}
         >
